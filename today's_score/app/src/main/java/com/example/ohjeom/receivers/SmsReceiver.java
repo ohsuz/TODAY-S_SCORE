da1309dfcc22d55a2f94aae166a4453902709624
Jang355 <iiss1010@naver.com>
2021-01-17 18:13:41 +0900

    :heavy_plus_sign: add PaymentService & :hammer: modify appNames

---
+++ today's_score/app/src/main/java/com/example/ohjeom/receivers/SmsReceiver.java
@@ -0,0 +1,102 @@
+package com.example.ohjeom.receivers;
+
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import com.example.ohjeom.models.Template;
+import com.example.ohjeom.services.PaymentService;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.content.Context.ACTIVITY_SERVICE;
+
+public class SmsReceiver extends BroadcastReceiver {
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+    private static final String TAG ="SmsReceiver";
+    private int count = 1;
+
+    static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.d(TAG,"문자 수신 받기 시작");
+
+        //SMS_RECEIVED에 대한 액션일때 실행
+        if(intent.getAction().equals(SMS_RECEIVED)&&isServiceRunning("com.example.ohjeom.services.PaymentService",context)) {
+            Log.d(TAG, "onReceiver() 호출");
+            Log.d(TAG, String.valueOf(count));
+
+            //Bundle을 이용해서 메세지 내용을 가져옴
+            Bundle bundle = intent.getExtras();
+            SmsMessage[] messages = parseSmsMessage(bundle);
+
+            //메시지가 있을 경우 내용을 로그로 출력해 봄
+            if (messages.length > 0 && messages[0].getOriginatingAddress().equals("01012345678") && messages[0].getMessageBody().toString().contains("원")) {
+                //메시지의 내용을 가져오기
+                String sender = messages[0].getOriginatingAddress();
+                String contents = messages[0].getMessageBody().toString();
+                Date receivedDate = new Date(messages[0].getTimestampMillis());
+
+                //로그 출력
+                Log.d(TAG, "Sender:" + sender);
+                Log.d(TAG, "contents:" + contents);
+                Log.d(TAG, "receivedDate:" + receivedDate);
+
+                //액티비티로 메시지의 내용을 전달해줌
+                sendToService(context, sender, contents, receivedDate);
+            }
+        }
+        else
+            Log.d("소비 측정","X");
+    }
+
+    //액티비티로 메시지의 내용 전달
+    private void sendToService(Context context,String sender,String contents, Date receivedDate) {
+        Intent intent = new Intent(context, PaymentService.class);
+
+        //Flag 설정
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        //메시지의 내용을 Extra에 넣어줌
+        intent.putExtra("count",count);
+        intent.putExtra("sender",sender);
+        intent.putExtra("contents",contents);
+        intent.putExtra("receivedDate",format.format(receivedDate));
+
+        context.startService(intent);
+    }
+
+    private SmsMessage[] parseSmsMessage(Bundle bundle) {
+        Object[] objs = (Object[]) bundle.get("pdus");
+        SmsMessage[] messages = new SmsMessage[objs.length];
+
+        for (int i = 0; i < objs.length; i++) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                String format = bundle.getString("format");
+                messages[i] = SmsMessage.createFromPdu((byte[]) objs[i], format);
+            } else {
+                messages[i] = SmsMessage.createFromPdu((byte[]) objs[i]);
+            }
+        }
+        return messages;
+    }
+
+    //서비스 작동 여부 체크
+    public boolean isServiceRunning(String serviceName,Context context) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo runServiceInfo : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceName.equals(runServiceInfo.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ today's_score/app/src/main/java/com/example/ohjeom/services/PaymentService.java
@@ -0,0 +1,134 @@
+package com.example.ohjeom.services;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.ohjeom.MainActivity;
+import com.example.ohjeom.R;
+import com.example.ohjeom.models.AccountSummary;
+import com.example.ohjeom.models.Payment;
+
+import java.util.ArrayList;
+import java.util.Timer;
+
+public class PaymentService extends Service {
+    public static int total;
+    public static ArrayList<Payment> payments;
+    private int amount = 0;
+    private int money;
+    private String usage;
+    private boolean next = false;
+    private Payment payment;
+    private AccountSummary accountSummary;
+
+    public PaymentService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Intent clsIntent = new Intent(PaymentService.this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(PaymentService.this, 0, clsIntent, 0);
+
+        NotificationCompat.Builder clsBuilder;
+        if (Build.VERSION.SDK_INT >= 26) {
+            String CHANNEL_ID = "channel_id";
+            NotificationChannel clsChannel = new NotificationChannel(CHANNEL_ID, "서비스 앱", NotificationManager.IMPORTANCE_DEFAULT);
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(clsChannel);
+
+            clsBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        } else {
+            clsBuilder = new NotificationCompat.Builder(this);
+        }
+
+        // QQQ: notification 에 보여줄 타이틀, 내용을 수정한다.
+        clsBuilder.setSmallIcon(R.drawable.icon_school)
+                .setContentTitle("서비스 앱").setContentText("서비스 앱")
+                .setContentIntent(pendingIntent);
+
+        // foreground 서비스로 실행한다.
+        startForeground(1, clsBuilder.build());
+
+        total = 0;
+        payments = new ArrayList<Payment>();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //처음 실행 시
+        if(startId==1) {
+            Log.d("소비 검사","시작");
+            money = intent.getIntExtra("money",0);
+            return super.onStartCommand(intent, flags, startId);
+        }
+        else {
+            String message = intent.getStringExtra("contents");
+            String[] array = message.split(" ");
+
+            for (String s : array) {
+                Log.d("dd", s);
+                if (s.contains("원") && amount == 0) {
+                    s = s.replace("원", "");
+                    amount = Integer.parseInt(s.replace(",", ""));
+                    total += amount;
+                    Log.d("현재까지 총합:", String.valueOf(total));
+                }
+                if (next) {
+                    usage = s;
+                    next = false;
+                    break;
+                }
+                if (s.contains(":")) {
+                    next = true;
+                }
+            }
+
+            payment = new Payment(usage, amount);
+            payments.add(payment);
+            amount = 0;
+
+            return super.onStartCommand(intent, flags, startId);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("소비 검사","종료");
+
+        /*
+        서버로 점수 보내기 및 총 사용량 & 사용내역 보내기 : score,accountSummary 보내면 됨
+         */
+        int score = 0;
+        accountSummary = new AccountSummary(total, payments);
+
+        Log.d("합계:", String.valueOf(total));
+        for (Payment p : accountSummary.getDetails()) {
+            Log.d("사용내역 : ", p.getUsage() + p.getAmount() + "원");
+        }
+
+        if (total < money)
+            score = 100;
+        else
+            score = 0;
+        Log.d("소비 점수:", String.valueOf(score));
+
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
