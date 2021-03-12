f1d5ed1297da384acf07e791fa5891bdee6de3c8
Jang355 <iiss1010@naver.com>
2021-03-12 13:07:36 +0900

    :add: add LoginActivity & Statistics

---
+++ today's_score/app/src/main/java/com/example/ohjeom/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.example.ohjeom;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //자동 로그인
+        SharedPreferences user = getSharedPreferences("user",MODE_PRIVATE);
+        if(user.getBoolean("Login",false)){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        EditText idEditText = findViewById(R.id.id_text);
+        EditText passwordEditText = findViewById(R.id.password_text);
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = idEditText.getText().toString();
+                String pwd = passwordEditText.getText().toString();
+                //원래는 id와 pwd를 서버로 보내서 로그인 성공 응답을 받아와야 함.
+                if(id.equals("aaa") && pwd.equals("1234")){ //로그인 성공했다면
+                    SharedPreferences user = getSharedPreferences("user",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = user.edit();
+                    editor.putString("id",id);
+                    editor.putBoolean("Login",true);
+                    editor.commit();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
